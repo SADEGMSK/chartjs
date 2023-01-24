fb76a6deb37ceb21a69fa268dbbd25a1335e7581
SADEGMSK <sadegh.kordi.1382@gmail.com>
2023-01-24 10:59:32 +0330

    at last we fix it

--- chart2.js
+++ chart2.js
@@ -1,5 +1,5 @@
-var xValues = []
-var n = 0;
+var xValues = [0 , 10]
+var n = 2;
 setInterval(() => { 
   let label = n*10
   xValues.push(label)
@@ -11,19 +11,19 @@ const chart1 = new Chart(ctx, {
  data: {
    labels: xValues,
    datasets: [{
-     data: [860,1140,1000,1060,1070,1110,1330,2210,7830,2478],
+     data: [],
      borderColor: "red",
      fill: false,
      showLine: true,
      spanGap: true,
    },{
-     data: [1600,1700,1800,1900,2000,2700,4000,5000,6000,7000],
+     data: [],
      borderColor: "green",
      fill: false,
      showLine: true,
      spanGap: true,
    },{
-     data: [300,700,2000,5000,6000,4000,2000,1005,200,100],
+     data: [],
      borderColor: "blue",
      fill: false,
      showLine: true,
@@ -37,12 +37,21 @@ const chart1 = new Chart(ctx, {
  }
 });
 
-function addData(chart, label, data , data , data) {
-   chart.data.labels.push(label);
-   chart.data.datasets.forEach((dataset) => {
-       dataset.data.push(data);
-   });
-   chart.update();
+function addData(oldData) {
+  var labels = oldData.data.labels;
+  var dataSetA = oldData["datasets"][0]["data"];
+  var dataSetB = oldData["datasets"][1]["data"];
+
+  labels.shift();
+  count++;
+  labels.push(count.toString());
+  var newDataA = dataSetA[9] + (20 - Math.floor(Math.random() * (41)));
+  var newDataB = dataSetB[9] + (20 - Math.floor(Math.random() * (41)));
+  dataSetA.push(newDataA);
+  dataSetB.push(newDataB);
+  dataSetA.shift();
+  dataSetB.shift();   
+
 }
 let random1;
 let random2;
@@ -51,19 +60,19 @@ setInterval(() => {
   random1= Math.floor(Math.random() * 8001)
   random2= Math.floor(Math.random() * 8001)
   random3= Math.floor(Math.random() * 8001)
-   addData(chart1,...xValues,random1 , random2 , random3)
-}, 3000);
+  chart1.data.datasets[0].data[5] = random1;
+  chart1.data.datasets[1].data[5] = random2;
+  chart1.data.datasets[2].data[5] = random3;
+  chart1.data.labels[5] = "Newly Added";
+  chart1.update();}, 3000);
 
-function removeData(chart,label,data) {
-   chart.data.labels.pop(label);
+function removeData(chart) {
+   chart.data.labels.pop();
    chart.data.datasets.forEach((dataset) => {
-      dataset.data.pop(data);
+      dataset.data.shift();
    });
    chart.update();
 }
 setInterval(() => {
-    random1= Math.floor(Math.random() * 8001)
-    random2= Math.floor(Math.random() * 8001)
-    random3= Math.floor(Math.random() * 8001)
- removeData(chart1,xValues,random1 , random2 , random3)
+ removeData(chart1)
 }, 3000);
